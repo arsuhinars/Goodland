6346146a820b08d1d702e48fd1c6743eb06f1ffc
arsuhinars <arsuhinars@gmail.com>
2023-06-21 18:01:45 +0400

    ✨ Utils class update

--- Assets/Scripts/Utils/Utils.cs
+++ Assets/Scripts/Utils/Utils.cs
@@ -1,9 +1,20 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public static class Utils
 {
     public static bool LayerMaskContainsLayer(LayerMask mask, int layer)
     {
         return mask == (mask | (1 << layer));
+    }
+
+    public static IDictionary<K, V> CreateDictionaryFromItems<K, V>(ICollection<SerializedKeyValuePair<K, V>> collection)
+    {
+        var dict = new Dictionary<K, V>();
+        foreach (var item in collection)
+        {
+            dict.Add(item.key, item.value);
+        }
+        return dict;
     }
 }
