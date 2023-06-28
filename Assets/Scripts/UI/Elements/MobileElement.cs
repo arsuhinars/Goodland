abd9b742c61296e3a185c344558f69979ca67812
arsuhinars <arsuhinars@gmail.com>
2023-06-28 22:32:41 +0400

    ✨ Added MobileElement class

---
+++ Assets/Scripts/UI/Elements/MobileElement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UIElements;
+
+public class MobileElement : VisualElement
+{
+    public new class UxmlFactory : UxmlFactory<MobileElement, UxmlTraits> { }
+
+    public MobileElement()
+    {
+        RegisterCallback<AttachToPanelEvent>(OnAttachedToPanel);
+    }
+
+    private void OnAttachedToPanel(AttachToPanelEvent ev)
+    {
+        style.display =
+            (Application.isMobilePlatform || Application.isEditor)
+            ? DisplayStyle.Flex
+            : DisplayStyle.None;
+    }
+}
