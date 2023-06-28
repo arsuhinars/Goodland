be1374af1af3b9e3a4aa13ed4fcbe3d895c81df7
arsuhinars <arsuhinars@gmail.com>
2023-06-28 20:14:14 +0400

    ✨ Added NavigationButton UI element

---
+++ Assets/Scripts/UI/Elements/NavigationButton.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine.UIElements;
+
+public class NavigationButton : Button
+{
+    public new class UxmlFactory : UxmlFactory<NavigationButton, UxmlTraits> { }
+
+    public new class UxmlTraits : Button.UxmlTraits
+    {
+        UxmlStringAttributeDescription m_ViewName = new()
+        {
+            name = "view-name"
+        };
+
+        public override void Init(VisualElement ve, IUxmlAttributes bag, CreationContext cc)
+        {
+            base.Init(ve, bag, cc);
+
+            (ve as NavigationButton).ViewName = m_ViewName.GetValueFromBag(bag, cc);
+        }
+    }
+
+    private string m_viewName;
+
+    public string ViewName
+    {
+        get => m_viewName;
+        set => m_viewName = value;
+    }
+
+    public NavigationButton() : base()
+    {
+        clicked += OnClick;
+    }
+
+    private void OnClick()
+    {
+        UIViewsManager.Instance.SetView(m_viewName);
+    }
+}
